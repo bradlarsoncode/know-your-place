edc09c7df151d3b1011a189bf66f63ba4491e16b
Brad Larson <larsonbrad@pm.me>
2021-03-17 14:07:57 -0400

    frontend only

--- server.js
+++ server.js
@@ -1,66 +1,66 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const User = require('./models/user');
-//const { restart } = require('nodemon');
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const User = require('./models/user');
+// //const { restart } = require('nodemon');
 
-const app = express();
+// const app = express();
 
-app.use(express.urlencoded({ extended: false}));
-app.use(express.json({ extended: false}));
+// app.use(express.urlencoded({ extended: false}));
+// app.use(express.json({ extended: false}));
 
-const dbUrl = require('./config/keys').mongoURI;
+// const dbUrl = require('./config/keys').mongoURI;
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(dbUrl, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        })
-        console.log("MongoDB is connected");
-    } catch (error) {
-        console.log(error);
-    }
-}
-//connect DB
-connectDB();
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(dbUrl, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//             useCreateIndex: true,
+//             useFindAndModify: false
+//         })
+//         console.log("MongoDB is connected");
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// //connect DB
+// connectDB();
 
-app.post("/api/register", async(req, res) => {
-    try {
-        await User.create({
-            name: req.body.userName,
-            email: req.body.userEmail,
-            description: req.body.userDescription
-        });
-        res.json({
-        message: 'User Registered'
-    })
-    } catch (error) {
-        console.log(error)
-        res.json({
-        message: 'Email Already Exists'
-    })
-    }
+// app.post("/api/register", async(req, res) => {
+//     try {
+//         await User.create({
+//             name: req.body.userName,
+//             email: req.body.userEmail,
+//             description: req.body.userDescription
+//         });
+//         res.json({
+//         message: 'User Registered'
+//     })
+//     } catch (error) {
+//         console.log(error)
+//         res.json({
+//         message: 'Email Already Exists'
+//     })
+//     }
 
     
-})
+// })
 
-app.get("/api/users", async(req, res) => {
-    try {
-        const users = await User.find();
+// app.get("/api/users", async(req, res) => {
+//     try {
+//         const users = await User.find();
 
-        res.json({
-            users: users
-        })
-    } catch (error) {
-        console.log(error)
-    }
-})
+//         res.json({
+//             users: users
+//         })
+//     } catch (error) {
+//         console.log(error)
+//     }
+// })
 
 
-const Port = 5000;
+// const Port = 5000;
 
-app.listen( Port, () => {
-    console.log("Server running on Port " + Port);
-})
+// app.listen( Port, () => {
+//     console.log("Server running on Port " + Port);
+// })
